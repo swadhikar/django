cb525fa25e342a81c95d03218c041148fe89c150
swadhi <cswadhikar@gmail.com>
2019-02-06 22:42:59 +0530

    database content

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Swadhi',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': '3, February 2019'
-    },
-    {
-        'author': 'Dilip',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '4, February 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
