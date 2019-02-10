e7d8376cd4835d10e418717c7b62ff1426c7bf14
swadhi <cswadhikar@gmail.com>
2019-02-10 18:30:36 +0530

    Login/Logout page

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -12,7 +13,12 @@ def register(request):
     if form.is_valid():
         form.save()
         username = form.cleaned_data.get('username')
-        messages.success(request, 'Account created for {}!'.format(username))
-        return redirect('blog-home')
+        messages.success(request, 'Your account has been created! You can now be able to log in'.format(username))
+        return redirect('login')
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
