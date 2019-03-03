bac9064d4e830682ee65a6f2d99a8ddab18a8fa6
swadhi <cswadhikar@gmail.com>
2019-03-03 17:09:11 +0530

    Create/Update/Delete post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -29,13 +35,37 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    fields = [
-        'title',
-        'content'
-    ]
+    success_url = '/'
+
+    # Override UserPassesTextMixin to check if current
+    # user is logged in
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+
+        # Check if requesting user is the post's owner
+        if self.request.user == post.author:
+            return True
+        return False
